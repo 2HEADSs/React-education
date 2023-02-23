e23546c0cddd09d429e2780e5dae272ebbe06a50
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-02-23 18:03:32 +0200

    update JSX demo

--- 03. Intro to React and JSX/React JSX- from lecture/build/app.js
+++ 03. Intro to React and JSX/React JSX- from lecture/build/app.js
@@ -1,38 +1,42 @@
-var rootElement = document.getElementById('root');
+var rootElement = document.getElementById("root");
 var root = ReactDOM.createRoot(rootElement);
 
 // const headingElement = React.createElement('h1', {}, 'Hello from React!');
 // const secondHeadingElement = React.createElement('h2', {}, 'Some slogan here');
 // const headerElement = React.createElement('header', {}, headingElement, secondHeadingElement);
-// Use JSX Syntax 
+// Use JSX Syntax
+
+var Heading = function Heading(props) {
+  return React.createElement(
+    "h1",
+    { className: "heading" },
+    "Hello from React!"
+  );
+};
 
 var headerElement = React.createElement(
-    "div",
-    null,
+  "div",
+  null,
+  React.createElement(
+    "header",
+    { className: "header-container" },
+    React.createElement(Heading, null),
     React.createElement(
-        "header",
-        { className: "header-container" },
-        React.createElement(
-            "h1",
-            { className: "heading" },
-            "Hello from React!"
-        ),
-        React.createElement(
-            "h2",
-            null,
-            "Slogan here"
-        ),
-        React.createElement(
-            "p",
-            null,
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consequuntur architecto dolorum sit quidem tenetur doloremque aspernatur reprehenderit ratione sed!"
-        )
+      "h2",
+      null,
+      "Slogan here"
     ),
     React.createElement(
-        "button",
-        null,
-        "Click"
+      "p",
+      null,
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consequuntur architecto dolorum sit quidem tenetur doloremque aspernatur reprehenderit ratione sed!"
     )
+  ),
+  React.createElement(
+    "button",
+    null,
+    "Click"
+  )
 );
 
 root.render(headerElement);
--- 03. Intro to React and JSX/React JSX- from lecture/src/app.js
+++ 03. Intro to React and JSX/React JSX- from lecture/src/app.js
@@ -1,21 +1,29 @@
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 // const headingElement = React.createElement('h1', {}, 'Hello from React!');
 // const secondHeadingElement = React.createElement('h2', {}, 'Some slogan here');
 // const headerElement = React.createElement('header', {}, headingElement, secondHeadingElement);
-// Use JSX Syntax 
+// Use JSX Syntax
+
+const Heading = (props) => {
+  return <h1 className="heading">Hello from React!</h1>;
+};
 
 const headerElement = (
-    <div>
-        <header className="header-container">
-            <h1 className="heading">Hello from React!</h1>
-            <h2>Slogan here</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis consequuntur architecto dolorum sit quidem tenetur doloremque aspernatur reprehenderit ratione sed!</p>
-        </header>
+  <div>
+    <header className="header-container">
+        <Heading/>
+      <h2>Slogan here</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+        consequuntur architecto dolorum sit quidem tenetur doloremque aspernatur
+        reprehenderit ratione sed!
+      </p>
+    </header>
 
-        <button>Click</button>
-    </div>
+    <button>Click</button>
+  </div>
 );
 
 root.render(headerElement);
