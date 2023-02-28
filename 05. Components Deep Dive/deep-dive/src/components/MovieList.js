16fc92b48ae01c32ab60d17c0bddbe80786a8f5e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-02-28 10:33:09 +0200

    update demo with different type of pass data

--- 05. Components Deep Dive/deep-dive/src/components/MovieList.js
+++ 05. Components Deep Dive/deep-dive/src/components/MovieList.js
@@ -1,6 +1,35 @@
 import Movie from "./Movie";
 
 export default function MovieList({ movies }) {
-    const firstMovie = movies[0];
-    return <Movie {...firstMovie} />;
+    // first way with desctructuring
+    // const firstMovie = movies[0];
+    // return <Movie {...firstMovie} />;
+
+    // second way with array of movies - wrong way
+    // return [
+    //     <Movie {...movies[0]} />,
+    //     <Movie {...movies[1]} />,
+    //     <Movie {...movies[2]} />,
+
+    // ]
+
+    //third way
+    // let movieList = movies.map(movie => <li> <Movie {...movie}/> </li>);
+
+    // return(
+    //     <ul>
+    //         {movieList}
+    //     </ul>
+    // )
+
+    //right way
+    return (
+        <ul>
+            {movies.map((movie) => (
+                <li>
+                    <Movie {...movie} />
+                </li>
+            ))}
+        </ul>
+    );
 }
--- 05. Components Deep Dive/deep-dive/src/movies.js
+++ 05. Components Deep Dive/deep-dive/src/movies.js
@@ -24,18 +24,6 @@ export const genres = [
 
 export const movies = [
     {
-        id: 1,
-        title: "Beetlejuice",
-        year: "1988",
-        runtime: "92",
-        genres: ["Comedy", "Fantasy"],
-        director: "Tim Burton",
-        actors: "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page",
-        plot: 'A couple of recently deceased ghosts contract the services of a "bio-exorcist" in order to remove the obnoxious new owners of their house.',
-        posterUrl:
-            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg",
-    },
-    {
         id: 2,
         title: "The Cotton Club",
         year: "1984",
@@ -48,18 +36,6 @@ export const movies = [
             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg",
     },
     {
-        id: 3,
-        title: "The Shawshank Redemption",
-        year: "1994",
-        runtime: "142",
-        genres: ["Crime", "Drama"],
-        director: "Frank Darabont",
-        actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
-        plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        posterUrl:
-            "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg",
-    },
-    {
         id: 4,
         title: "Crocodile Dundee",
         year: "1986",
@@ -70,18 +46,6 @@ export const movies = [
         plot: "An American reporter goes to the Australian outback to meet an eccentric crocodile poacher and invites him to New York City.",
         posterUrl:
             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg",
-    },
-    {
-        id: 5,
-        title: "Valkyrie",
-        year: "2008",
-        runtime: "121",
-        genres: ["Drama", "History", "Thriller"],
-        director: "Bryan Singer",
-        actors: "Tom Cruise, Kenneth Branagh, Bill Nighy, Tom Wilkinson",
-        plot: "A dramatization of the 20 July assassination and political coup plot by desperate renegade German Army officers against Hitler during World War II.",
-        posterUrl:
-            "http://ia.media-imdb.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_SX300.jpg",
     },
     {
         id: 6,
