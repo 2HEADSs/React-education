3f23f4045f830caa5d7ac3aaf54ba4a6dfad30a5
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 10:25:56 +0200

    add footer component

--- 06. Workshop Components -2022/client/src/App.js
+++ 06. Workshop Components -2022/client/src/App.js
@@ -1,9 +1,11 @@
-import { Header } from "./components/Header";
+import { Header } from "./components/common-components/Header";
 import './App.css'
+import { Footer } from "./components/common-components/Footer";
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
         </div>
     );
 }
