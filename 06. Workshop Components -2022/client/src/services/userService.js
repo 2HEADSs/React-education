4fcfa1e8e1cddeaa2bdfb20bc7850e6c10145088
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 18:59:07 +0200

    add create service

--- 06. Workshop Components -2022/client/src/services/userService.js
+++ 06. Workshop Components -2022/client/src/services/userService.js
@@ -1,17 +1,32 @@
 const baseUrl = 'http://localhost:3005/api';
 
 
-export const getAll = async()=>{
+export const getAll = async () => {
     const response = await fetch(`${baseUrl}/users`);
     const result = await response.json();
-     
-    return result.users 
+
+    return result.users
 }
 
 
-export const getOne = async(userId)=>{
+export const getOne = async (userId) => {
     const response = await fetch(`${baseUrl}/users/${userId}`);
     const result = await response.json();
-     
+
     return result.user
+}
+
+export const create = async (userData) => {
+    const response = await fetch(`${baseUrl}/users`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+    });
+    const result = await response.json();
+
+    console.log(result);
+
+    return result
 }
