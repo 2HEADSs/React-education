bf1178fb5030546c3df2b6ce23cbd26ba783b421
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 10:38:16 +0200

    add user-section

--- 06. Workshop Components -2022/client/src/App.js
+++ 06. Workshop Components -2022/client/src/App.js
@@ -1,10 +1,21 @@
 import { Header } from "./components/common-components/Header";
 import './App.css'
 import { Footer } from "./components/common-components/Footer";
+import { Search } from "./components/search/Search";
+import { UserSection } from "./components/user-section/UserSection";
 function App() {
     return (
         <div className="App">
             <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+                    <UserSection/>
+                </section>
+
+            </main>
+
             <Footer />
         </div>
     );
