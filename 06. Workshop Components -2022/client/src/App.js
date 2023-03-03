a966783291d63bb9f379be9a1db0949535f04c8b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 10:12:18 +0200

    add css file nad create header components

--- 06. Workshop Components -2022/client/src/App.js
+++ 06. Workshop Components -2022/client/src/App.js
@@ -1,9 +1,9 @@
-
+import { Header } from "./components/Header";
 
 function App() {
     return (
         <div className="App">
-            <h1>Users</h1>
+            <Header />
         </div>
     );
 }
