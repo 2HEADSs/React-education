9db62669243fe74a64717b8641e2b11d3baf1ea1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 11:55:11 +0200

    create fetch for data

--- 06. Workshop Components -2022/client/src/App.js
+++ 06. Workshop Components -2022/client/src/App.js
@@ -3,17 +3,33 @@ import './App.css'
 import { Footer } from "./components/common-components/Footer";
 import { Search } from "./components/search/Search";
 import { UserSection } from "./components/user-section/UserSection";
+import { useEffect, useState } from "react";
+
+const baseUrl = 'http://localhost:3005/api';
+
 function App() {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch(`${baseUrl}/users`)
+            .then(res => res.json())
+            .then(result => {
+                setUsers(result.users)
+            })
+    }, []);
+    console.log(users);
+
     return (
         <div className="App">
             <Header />
 
             <main className="main">
                 <section className="card users-container">
-                    
+
                     <Search />
-                    
-                    <UserSection/>
+
+                    <UserSection user={users} />
                 </section>
 
             </main>
