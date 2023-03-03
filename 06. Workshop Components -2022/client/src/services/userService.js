eedf59c03c92a4d03f996ade157b8f45c1050715
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-03 14:14:41 +0200

    add new service functionality

--- 06. Workshop Components -2022/client/src/services/userService.js
+++ 06. Workshop Components -2022/client/src/services/userService.js
@@ -4,7 +4,14 @@ const baseUrl = 'http://localhost:3005/api';
 export const getAll = async()=>{
     const response = await fetch(`${baseUrl}/users`);
     const result = await response.json();
-    console.log(result);
      
     return result.users 
+}
+
+
+export const getOne = async(userId)=>{
+    const response = await fetch(`${baseUrl}/users/${userId}`);
+    const result = await response.json();
+     
+    return result.user
 }
