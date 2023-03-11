bb195534ed13292cf8efb9219828f2d8155361a9
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-11 17:14:46 +0200

    update demo

--- 07. Forms/react-forms-demo/src/App.js
+++ 07. Forms/react-forms-demo/src/App.js
@@ -1,13 +1,29 @@
-
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    const [username, setUsername] = useState('Pesho');
+    useEffect(()=>{
+        setTimeout(()=>{
+            setUsername('Gosho')
+        }, 3000)
+    },[])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form action="">
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" id="username" defaultValue={username} />
+                    </div>
+                    <div>
+                        <input type="submit" value="Send" />
+                    </div>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
