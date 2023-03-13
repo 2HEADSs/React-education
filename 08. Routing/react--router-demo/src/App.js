925d785c0367ef97a2642a194acb0c17996ad861
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-13 16:41:20 +0200

    update demo

--- 08. Routing/react--router-demo/src/App.js
+++ 08. Routing/react--router-demo/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/*' element={<h1>404</h1>} />
         </Routes>
       </header>
     </div >
---
+++ 08. Routing/react--router-demo/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import style from './Navigation.module.css'
+
+export const Navigation = () => {
+    return (
+        <nav className={style.navigation}>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li></li>
+        </ul>
+        </nav>
+    )
+}
