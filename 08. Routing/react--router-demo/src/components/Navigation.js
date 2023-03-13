6e48362cd65f012f0f4155b28b625777858805f6
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-13 16:53:17 +0200

    update with css

--- 08. Routing/react--router-demo/src/components/Navigation.js
+++ 08. Routing/react--router-demo/src/components/Navigation.js
@@ -1,8 +1,8 @@
-import style from './Navigation.module.css'
+import styles from './Navigation.module.css'
 
 export const Navigation = () => {
     return (
-        <nav className={style.navigation}>
+        <nav className={styles.navigation}>
         <ul>
             <li>Home</li>
             <li>About</li>
