efd9ddfc6ab800ff88805047cd6c4d4355d9f1c8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-14 21:31:22 +0200

    Update Catalog.js

--- 09. Workshop Routing/games-play-app/src/components/Catalog/Catalog.js
+++ 09. Workshop Routing/games-play-app/src/components/Catalog/Catalog.js
@@ -1,35 +1,15 @@
-export const Catalog = () => {
+import { CatalogItem } from "./CatalogItem/CatalogItem"
+
+export const Catalog = ({
+    games
+}) => {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
 
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
+            {games.map(x =>
+                <CatalogItem key={x._id} {...x} />
+            )}
             {/* <!-- Display paragraph: If there is no games  --> */}
             <h3 className="no-articles">No articles yet</h3>
         </section>
