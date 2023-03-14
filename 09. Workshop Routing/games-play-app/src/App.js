dcdba19df9ceb3aa3da3a1fc5e6dc5e9124cab7a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-14 13:23:41 +0200

    add footer component

--- 09. Workshop Routing/games-play-app/src/App.js
+++ 09. Workshop Routing/games-play-app/src/App.js
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/";
 
 
@@ -5,6 +6,7 @@ function App() {
     return (
         <div id="box">
             <Header />
+            <Footer />
         </div>
     );
 }
---
+++ 09. Workshop Routing/games-play-app/src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+export const Footer = () => {
+    return (
+        <div>All right reserved &copy;</div>
+    );
+}
