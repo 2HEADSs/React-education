be7afe511089b8bf143481dab3e278ea6d8ded75
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-14 16:39:45 +0200

    add catalog, login, home, register components

--- 09. Workshop Routing/games-play-app/src/App.js
+++ 09. Workshop Routing/games-play-app/src/App.js
@@ -1,11 +1,25 @@
+import { Catalog } from "./components/Catalog/Catalog";
+import { CreateGame } from "./components/CreateGame/CreateGame";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+
 
 
 function App() {
     return (
         <div id="box">
             <Header />
+            {/* <!-- Main Content --> */}
+            <main id="main-content">
+                <Home />
+                <Login />
+                <Register />
+                <CreateGame />
+                <Catalog/>
+            </main>
             <Footer />
         </div>
     );
