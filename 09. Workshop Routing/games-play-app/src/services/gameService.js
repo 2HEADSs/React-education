c095eb9d8ba68243e9c8622bc821fe801bbbad5c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-14 21:18:12 +0200

    fix gameService

--- 09. Workshop Routing/games-play-app/src/services/gameService.js
+++ 09. Workshop Routing/games-play-app/src/services/gameService.js
@@ -3,7 +3,7 @@ import * as request from "./requester";
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-    const result = await request.get('GET', baseUrl);
+    const result = await request.get(baseUrl);
     const games = Object.values(result)
     return games;
 };
