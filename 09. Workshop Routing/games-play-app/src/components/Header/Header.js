6e04721e9f13b7502b6aae3f01fb92b92062355d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-14 16:56:36 +0200

    add active navigation

--- 09. Workshop Routing/games-play-app/src/components/Header/Header.js
+++ 09. Workshop Routing/games-play-app/src/components/Header/Header.js
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom'
+
 export const Header = () => {
     return (
         <header>
             {/* <!-- Navigation --> */}
-            <h1><a className="home" href="#">GamesPlay</a></h1>
+            <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
-                <a href="#">All games</a>
+                <Link to="/catalog">All games</Link>
                 {/* <!-- Logged-in users --> */}
                 <div id="user">
-                    <a href="#">Create Game</a>
-                    <a href="#">Logout</a>
+                    <Link to="/create-game">Create Game</Link>
+                    <Link to="/logout">Logout</Link>
                 </div>
                 {/* <!-- Guest users --> */}
                 <div id="guest">
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
