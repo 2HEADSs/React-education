41eb53574cb28f654481f12b93263c10a5cbec89
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-15 18:58:00 +0200

    update catalogItem

--- 09. Workshop Routing/games-play-app/src/components/Catalog/CatalogItem/CatalogItem.js
+++ 09. Workshop Routing/games-play-app/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -1,14 +1,17 @@
 export const CatalogItem = ({
-    name,
-    imgUrl,
-    genre
+    title,
+    imageUrl,
+    category
 }) => {
+    console.log(    title,
+        imageUrl,
+        category);
     return (
         <div className="allGames">
             <div className="allGames-info">
-                <img src={imgUrl} alt={name} />
-                <h6>{genre}</h6>
-                <h2>{name}</h2>
+                <img src={imageUrl} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
                 <a href="#" className="details-button">Details</a>
             </div>
 
