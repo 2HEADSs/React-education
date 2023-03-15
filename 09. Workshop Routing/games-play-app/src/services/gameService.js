9b35e6c899a2614882d00ee660fddadfec983a77
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-15 23:18:45 +0200

    add comments

--- 09. Workshop Routing/games-play-app/src/services/gameService.js
+++ 09. Workshop Routing/games-play-app/src/services/gameService.js
@@ -9,12 +9,18 @@ export const getAll = async () => {
 };
 
 
-export const create = async (gameData) => {
-    const result = await request.post(baseUrl,gameData);
-    return result;
-};
-
 export const getOne = async (gameId) => {
     const result = await request.get(`${baseUrl}/${gameId}`);
     return result;
 };
+
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData);
+
+    return result
+};
+
+
+export const addComment = async (gameId, data)=>{
+    const result = await request.post(`${baseUrl}/${gameId}/comments`, data)
+}
