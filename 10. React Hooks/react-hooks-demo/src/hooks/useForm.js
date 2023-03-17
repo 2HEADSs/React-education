573dce2d37ae1898d7cd2418f200e6ca84be52e6
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-17 21:25:29 +0200

    Update useForm.js

--- 10. React Hooks/react-hooks-demo/src/hooks/useForm.js
+++ 10. React Hooks/react-hooks-demo/src/hooks/useForm.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react"
 
-export const useForm = () => {
+export const useForm = (initialValues) => {
+    const [formValues, setFormValues] = useState({ initialValues });
 
+    const onChangeHandler = (e) => { 
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}))
+    }
 }
