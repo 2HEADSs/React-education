9934786966902f7020e17876296e33c433065611
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-17 21:18:34 +0200

    Update AddTodoModal.js

--- 10. React Hooks/react-hooks-demo/src/components/AddTodoModal.js
+++ 10. React Hooks/react-hooks-demo/src/components/AddTodoModal.js
@@ -1,11 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 export const AddTodoModal = () => {
-    const onTodoSubmit = (e) => { 
+    const [formValues, setFormValues] = useState({
+        text: '',
+    })
+    const onChangeHandler = (e) => {
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}))
+    }
+    const onTodoSubmit = (e) => {
         e.preventDefault();
-        console.log('eeee');
+        console.log(formValues);
     }
     return (
         <Modal show={true}>
@@ -17,18 +24,16 @@ export const AddTodoModal = () => {
                 <Form onSubmit={onTodoSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Todo</Form.Label>
-                        <Form.Control type="text" placeholder="Do the dishes" />
+                        <Form.Control type="text" name="text" placeholder="Do the dishes"
+                        value={formValues.name} onChange={onChangeHandler}/>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" style={{ marginRight: '10px' }}>
                         Submit
                     </Button>
+
+                    <Button variant="secondary">Close</Button>
                 </Form>
             </Modal.Body>
-
-            <Modal.Footer>
-                <Button variant="secondary">Close</Button>
-
-            </Modal.Footer>
         </Modal >
     )
 }
