5515e9976671af11f0ff20964949ac671ce18eb1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-21 22:00:51 +0200

    Update requester.js

--- 09. Workshop Routing/games-play-app/src/services/requester.js
+++ 09. Workshop Routing/games-play-app/src/services/requester.js
@@ -11,20 +11,21 @@ const request = async (method, url, data) => {
 
             options.body = JSON.stringify(data)
         }
-    }
+    };
 
     const response = await fetch(url, options);
-    if (!response.ok) {
-        const result = await response.json();
-        throw result;
-    }
 
-    try {
-        const result = await response.json();
-        return result;
-    } catch (error) {
+    if (response.status === 204) {
         return {}
-    }
+    };
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    };
+
+    return result;
 };
 
 
