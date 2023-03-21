d85e7878b2723846bba5a382ab4b45849c7f00a4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-21 19:40:43 +0200

    Update Login.js

--- 09. Workshop Routing/games-play-app/src/components/Login/Login.js
+++ 09. Workshop Routing/games-play-app/src/components/Login/Login.js
@@ -1,19 +1,40 @@
-import { useContext } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForms";
+
+const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password'
+}
 
 export const Login = () => {
     const { onLoginSubmit } = useContext(AuthContext)
+    const { values, changeHandler, onSubmit} = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: ''
+    }, onLoginSubmit)
     return (
         <section id="login-page" className="auth">
-            <form id="login" onSubmit={onLoginSubmit}>
+            <form id="login" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input
+                        type="email"
+                        id="email"
+                        name={LoginFormKeys.Email}
+                        placeholder="Sokka@gmail.com"
+                        value={values[LoginFormKeys.Email]}
+                        onChange={changeHandler} />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input
+                        type="password"
+                        id="login-password"
+                        name={LoginFormKeys.Password}
+                        value={values[LoginFormKeys.Password]}
+                        onChange={changeHandler} />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
