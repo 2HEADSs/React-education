1981dbfcc64de565723f3572e9b4d244a8f8ac39
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-21 19:48:30 +0200

    Update authService.js

--- 09. Workshop Routing/games-play-app/src/services/authService.js
+++ 09. Workshop Routing/games-play-app/src/services/authService.js
@@ -1,13 +1,19 @@
-import { requestFactory } from './requester';
+// import { requestFactory } from './requester';
+import * as request from './requester';
 
 const baseUrl = `http://localhost:3030/users`;
 
-export const authServiceFactory = (token) => {
-    const request = requestFactory(token);
+// export const authServiceFactory = (token) => {
+//     const request = requestFactory(token);
 
-    return {
-        login: (data) => request.post(`${baseUrl}/login`, data),
-        register: (data) => request.post(`${baseUrl}/register`, data),
-        logout: () => request.get(`${baseUrl}/logout`),
-    }
-};
+//     return {
+//         login: (data) => request.post(`${baseUrl}/login`, data),
+//         register: (data) => request.post(`${baseUrl}/register`, data),
+//         logout: () => request.get(`${baseUrl}/logout`),
+//     }
+// };
+
+
+export const login = (loginData) => request.post(`${baseUrl}/login`, loginData)
+
+
