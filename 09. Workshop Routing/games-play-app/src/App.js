7d569e3b61c15eedac49b1ec9ca55881496ee4bb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-22 19:27:20 +0200

    add logout

--- 09. Workshop Routing/games-play-app/src/App.js
+++ 09. Workshop Routing/games-play-app/src/App.js
@@ -10,6 +10,7 @@ import { CreateGame } from "./components/CreateGame/CreateGame";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/";
 import { Home } from "./components/Home/Home";
+import { Logout } from './components/Logout/Logout';
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
 import { GameDetails } from './components/GameDetails/GameDetails';
@@ -60,16 +61,24 @@ function App() {
             console.log('There is a problem with register');
 
         }
+    };
+
+    const onLogout = async () => {
+        //TODO :AUTHORIZED REQUEST
+        // await authService.logout();
+
+        setAuth({});
     }
 
     const contenxt = {
         onLoginSubmit,
         onRegisterSubmit,
+        onLogout,
         userId: auth._id,
         token: auth.accessToken,
         userEmail: auth.email,
         isAuthenticated: !!auth.accessToken
-    }
+    };
 
 
     return (
@@ -82,6 +91,7 @@ function App() {
 
                         <Route path='/' element={<Home />} />
                         <Route path='/login' element={<Login />} />
+                        <Route path='/logout' element={<Logout />} />
                         <Route path='/register' element={<Register />} />
                         <Route path='/create-game' element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
                         <Route path='/catalog' element={<Catalog games={games} />} />
--- 09. Workshop Routing/games-play-app/src/services/authService.js
+++ 09. Workshop Routing/games-play-app/src/services/authService.js
@@ -16,5 +16,6 @@ const baseUrl = `http://localhost:3030/users`;
 
 export const login = (data) => request.post(`${baseUrl}/login`, data);
 export const register = (data) => request.post(`${baseUrl}/register`, data);
+export const logout = () => request.post(`${baseUrl}/logout`);
 
 
